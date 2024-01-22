5b33ca7986e059bdb3a0ef4408700b970bb8405a
niiquash <qua16006@byui.edu>
2024-01-22 14:50:31 -0700

    build home page hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import "./home.css";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="home-container">
+      <div className="home-wrapper">
+        <div className="hero-img-container">
+          <img
+            className="hero-img"
+            src="/assets/hanging-gloves.jpg"
+            alt="boxing"
+          />
+          <div className="hero-text-wrapper">
+            <h4 className="hero-text">Welcome to USA Utah Amateur Boxing</h4>
+          </div>
+          <div className="hero-link-container">
+            <a className="hero-link" href="#">
+              find out more
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
